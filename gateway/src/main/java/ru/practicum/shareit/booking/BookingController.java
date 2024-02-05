a6dd80a5bffd347937ced34110bb13b9b1e5efcd
saryc <dsarychev92@yandex.ru>
2024-02-05 18:48:23 +0300

    fix: tests

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -20,22 +19,44 @@ import javax.validation.constraints.PositiveOrZero;
 @Slf4j
 @Validated
 public class BookingController {
-    private final BookingClient bookingClient;
 
-    @GetMapping
-    public ResponseEntity<Object> getBookings(@RequestHeader("X-Sharer-User-Id") long userId,
-                                              @RequestParam(name = "state", defaultValue = "all") BookingState state,
-                                              @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
-                                              @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
-        log.info("Get booking with state {}, userId={}, from={}, size={}", state, userId, from, size);
-        return bookingClient.getBookings(userId, state, from, size);
-    }
+    private final BookingClient bookingClient;
 
     @PostMapping
     public ResponseEntity<Object> bookItem(@RequestHeader("X-Sharer-User-Id") long userId,
                                            @RequestBody @Valid BookItemRequestDto requestDto) {
         log.info("Creating booking {}, userId={}", requestDto, userId);
         return bookingClient.bookItem(userId, requestDto);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public ResponseEntity<Object> updateBooking(@RequestHeader("X-Sharer-User-Id") long userId,
+                                                @PathVariable long bookingId,
+                                                @RequestParam boolean approved) {
+        log.info("Updating booking {}, userId={}", bookingId, userId);
+        return bookingClient.updateBooking(userId, bookingId, approved);
+    }
+
+    @GetMapping("/owner")
+    public ResponseEntity<Object> getBookingsForOwnedItems(@RequestHeader("X-Sharer-User-Id") long userId,
+                                                           @RequestParam(name = "state", defaultValue = "all") String stateParam,
+                                                           @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
+                                                           @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
+        BookingState state = BookingState.from(stateParam)
+                .orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
+        log.info("Get bookings with state {}, userId={}, from={}, size={}", stateParam, userId, from, size);
+        return bookingClient.getBookingsByOwner(userId, state, from, size);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getBookings(@RequestHeader("X-Sharer-User-Id") long userId,
+                                              @RequestParam(name = "state", defaultValue = "all") String stateParam,
+                                              @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
+                                              @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
+        BookingState state = BookingState.from(stateParam)
+                .orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
+        log.info("Get bookings with state {}, userId={}, from={}, size={}", stateParam, userId, from, size);
+        return bookingClient.getBookings(userId, state, from, size);
     }
 
     @GetMapping("/{bookingId}")
--- gateway/src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
@@ -1,13 +1,13 @@
 package ru.practicum.shareit.handler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.exception.UnknownStateException;
 
 @Slf4j
 @RestControllerAdvice
@@ -21,11 +21,11 @@ public class ErrorHandler {
         return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler({UnknownStateException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleConversionFailedException(final ConversionFailedException e) {
+    public ErrorResponse handleUnknownStateException(final UnknownStateException e) {
         log.debug(e.getMessage());
-        return new ErrorResponse("Unknown state: " + e.getValue());
+        return new ErrorResponse("Unknown state: " + e.getMessage());
     }
 
     @ExceptionHandler
